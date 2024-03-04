9133310667c40f6898c8704cd95207488f2d4d5e
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-03-04 13:04:52 +0000

    setting up cloudinary

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,3 +1,8 @@
+# config/initializers/cloudinary.rb
+
 Cloudinary.config do |config|
-  
+  config.cloud_name = ENV.fetch('CLOUD_NAME')
+  config.api_key = ENV.fetch('API_KEY')
+  config.api_secret = ENV.fetch('API_SECRET')
+  config.secure = true # Optional: Use HTTPS for secure connections
 end
